c3e801d8dd766e77a1ab67e5f5932b49f9b65b72
chowdsan <sanjana.achowdhury@gmail.com>
2020-01-24 11:37:03 -0500

    new branch

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,10 @@
+package studentlist;
+
+/**
+ * This class represents
+ * @author Sanjana Chowdhury
+ */
+public class PartTimeStudent extends Student {
+    private int hoursOfStudy;
+    public PartTimeStudent(String givenName, Str Super(givenName, givenID));
+}
